a266c31fb8c389174729a4912482c90dc92a8adf
Dario Eduardo Suarez <dario.suarez@kavak.com>
2026-06-18 12:48:43 -0300

    upgrade kotlin config

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 
     sourceSets {
